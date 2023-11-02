cd672658c9aa17a732866abeda418d82c11dcbf5
BorisKGB <flot3.sky@yandex.ru>
2023-11-02 20:22:54 +0300

    describe basic animal logic

--- src/main/java/ru/study/nursery/model/NurseryService.java
+++ src/main/java/ru/study/nursery/model/NurseryService.java
@@ -1,4 +1,10 @@
 package ru.study.nursery.model;
 
+import java.util.Random;
+
 public class NurseryService {
+    public static int numberInRange(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min)+1) + min;
+    }
 }
--- src/main/java/ru/study/nursery/model/animal/Animal.java
+++ src/main/java/ru/study/nursery/model/animal/Animal.java
@@ -2,13 +2,30 @@ package ru.study.nursery.model.animal;
 
 import ru.study.nursery.model.command.Command;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import static ru.study.nursery.model.NurseryService.numberInRange;
 
 public abstract class Animal {
     private String name;
     private Date birthDate;
     private List<Command> commands;
+    private final short max_commands = 0;
+    private final byte learn_probability = 0;
+
+    public Animal(String name, Date bdate) {
+        this.name = name;
+        this.birthDate = bdate;
+        this.commands = new ArrayList<>();
+    }
+
+    public Animal(String name, Date bdate, List<Command> commands) {
+        this.name = name;
+        this.birthDate = bdate;
+        commands = new ArrayList<>();
+    }
 
     /**
      * Try to learn Command
@@ -17,6 +34,38 @@ public abstract class Animal {
      * @return learning result
      */
     public boolean learn(Command command) {
+        if (commands != null && commands.size() < max_commands && numberInRange(0, 100) > learn_probability) {
+            commands.add(command);
+            return true;
+        }
         return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public List<Command> getCommands() {
+        return commands;
+    }
+
+    public short getMax_commands() {
+        return max_commands;
+    }
+
+    public byte getLearn_probability() {
+        return learn_probability;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", commands=" + commands +
+                '}';
     }
 }
